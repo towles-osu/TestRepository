547bd5378ae75b636e192d401b4d0591ec0a131e
towles-osu <towles@oregonstate.edu>
2020-09-27 12:17:20 -0700

    update

--- experiment.py
+++ experiment.py
@@ -2,10 +2,8 @@
 #9/27/2020
 # This program is just a sandbox for playing with python and testing github
 
-
-playThing = input("what do you have? ")
-print(playThing + " is what you got. What shall we do with it?")
-result = input("?= ")
-print("okay, let's " + result + " " + playThing)
+playThing = input("what do you have?")
+result = input(playThing + " is what you got. What shall we do with your " + playThing + ("?"))
+print("okay, you got " + playThing + " and we're going to " + result)
 feelings = input("how did that make you feel? ")
 print("well I don't really care that you feel " + feelings + ("."))
