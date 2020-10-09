e82add858753034c582bebad42f4d3fa6dda6dfc
towles-osu <towles@oregonstate.edu>
2020-10-08 19:24:32 -0700

    update

--- experiment.py
+++ experiment.py
@@ -11,9 +11,9 @@ print("well I don't really care that you feel, " + feelings + ("."))
 #here we will attempt to create a loop that asks the user if they are bored
 #then, if they are not, asks another question
 bored = False
-while not bored:
-    isBored = input('Are you bored? Please answer "True" or "False" : ')
-    if isBored == 'False':
+while not(bored):
+    isBored = input('Are you bored? Please answer "Yes" or "No" : ')
+    if isBored == 'no' or isBored == '"No"' or isBored == 'No' or isBored == '"no"':
         print("So what's new?")
         answer = input()
         print("cool beans")
--- testing_sandbox.py
+++ testing_sandbox.py
@@ -9,3 +9,15 @@ for testb in range(0,5):
 
 for testc in range(7, 2):
     print(testc)
+
+for testd in range(10, 0, -1):
+    if(testd == 5):
+        continue
+    print(testd)
+
+
+
+for letter in "robot animal":
+    if letter == " ":
+        continue
+    print(letter)
