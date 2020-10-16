558ef2f34cf0b72455b84777c5c55dbd8cc13d6e
towles-osu <towles@oregonstate.edu>
2020-10-15 18:45:39 -0700

    update

---
+++ examply.py
@@ -0,0 +1,19 @@
+
+
+
+counter = 1
+
+def operation(num):
+    counter += 1
+    return num * 2
+
+print(counter)
+print(operation(counter))
+print(counter)
+print(operation(counter))
+print(counter)
+print(operation(counter))
+print(counter)
+print(operation(counter))
+print(counter)
+print(operation(counter))
